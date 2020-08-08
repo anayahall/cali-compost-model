bef789b89d416cc0595ba650c49c1c4afbfc4507
anayahall <anayahall@gmail.com>
2020-08-08 07:51:41 -0700

    add f2r non-regional

--- scripts/chordplotprep.py
+++ scripts/chordplotprep.py
@@ -3,12 +3,14 @@
 import pandas as pd
 import pickle
 import os
+from os.path import join as opj
 import geopandas as gpd
 
 from region import add_region_variable
 
 # read in data:
-# os.chdir('../results')
+DATA_DIR = "data"
+RESULTS_DIR = "results"
 
 print("loading data")
 with open('results/c2f_FG_50p.p', 'rb') as f:
@@ -54,7 +56,8 @@ c2fdf = c2fdf.groupby(['SWIS_County'], as_index = False).sum()
 
 print("reshaping")
 # # reshape!
-c2fdf_melted = pd.melt(c2fdf, id_vars=['SWIS_County'],var_name = 'MSW_County', value_name='value')
+c2fdf_melted = pd.melt(c2fdf, id_vars=['SWIS_County'],var_name = 'MSW_County', 
+    value_name='value')
 # c2fdf
 
 c2fdf = c2fdf_melted.groupby(['MSW_County', 'SWIS_County'], as_index = False).sum()
@@ -89,4 +92,64 @@ flow_reg_group = flow_reg.groupby(['from_region', 'to_region'], as_index = False
 flow_reg_group.to_csv('results/chord/C2F_FG_50_region.csv')
 
 
+
+
+##############################################################
+##############################################################
+
+
 ## next do for F2R! 
+frdf = pd.DataFrame.from_dict(f2r)
+frdf = frdf.reset_index()
+frdf.rename(columns={'index':'OBJECTID'},inplace=True)
+
+
+# Import rangelands
+# print("about to import rangelands") if (DEBUG == True) else ()
+rangelands = gpd.read_file(opj(DATA_DIR, "raw/CA_FMMP_G/gl_bycounty/grazingland_county.shp"))
+rangelands = rangelands.to_crs(epsg=4326) # make sure this is read in degrees (WGS84)
+
+# Fix county names in RANGELANDS! 
+countyIDs = pd.read_csv(opj(DATA_DIR, "interim/CA_FIPS_wcode.csv"), 
+	names = ['FIPS', 'COUNTY', 'State', 'county_nam'])
+countyIDs = countyIDs[['COUNTY', 'county_nam']]
+rangelands = pd.merge(rangelands, countyIDs, on = 'county_nam')
+# keep for using later or right away 
+rangelands = rangelands[['OBJECTID', 'COUNTY']]
+
+#RESHAPE
+frdf_melted = pd.melt(frdf, id_vars=['OBJECTID'], var_name = 'SwisNo', 
+    value_name = 'value')
+
+
+
+
+
+# merge back to get counties on frdf
+frdf_merged = pd.merge(frdf_melted, rangelands, on = 'OBJECTID')
+
+# get rid of zeros
+frdf_merged = frdf_merged[frdf_merged['value']!=0]
+
+# before reshaping collapse by COUNTY
+frdf_swisno = frdf_merged.groupby(['COUNTY', 'SwisNo'], as_index = False).sum()
+
+frdf_swisno.rename(columns={'COUNTY':'RL_County'}, inplace = True)
+
+frdf = pd.merge(frdf_swisno, swis, on = "SwisNo")
+frdf.rename(columns = {'County':'F_County'}, inplace = True)
+
+
+frdf = frdf.groupby(['F_County', 'RL_County'], as_index = False).sum()
+
+frdf = frdf[['F_County', 'RL_County', 'value']]
+
+
+
+
+
+
+
+
+
+
